cee6907ef47eb3d5f67c2ef50182a36f3dfb4614
Vishakha Tyagi <vishakha.tyagi@ip-10-159-193-28.ap-southeast-1.compute.internal>
2021-04-29 11:12:06 +0530

    full_requirements

--- Challenge-1/00_modules/02_ec2/user_data.sh
+++ Challenge-1/00_modules/02_ec2/user_data.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y httpd
-echo "Welcome to the JungleBook" > /var/www/html/index.html
-curl http://169.254.169.254/latest/meta-data/local-hostname > /var/www/html/index.html
-sudo systemctl start httpd
-sudo systemctl enable httpd
+
+
+#write commands here
